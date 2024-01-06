7370e8950ac840f10bfc8f2badeda6d2ead36fd5
EscuteloJr <escutelo@escutelo>
2024-01-06 15:48:19 +0100

    Estrutura Base do projecto. Criação dos serviços no docker base de dados : banco_engenharia_db servicos : banco-engenharia-service

---
+++ banco-engenharia-service/src/main/java/ucan/edu/bancoengenharia/BancoEngenhariaApplication.java
@@ -0,0 +1,13 @@
+package ucan.edu.bancoengenharia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoEngenhariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoEngenhariaApplication.class, args);
+	}
+
+}
---
+++ banco-engenharia-service/src/test/java/ucan/edu/bancoengenharia/BancoEngenhariaApplicationTests.java
@@ -0,0 +1,13 @@
+package ucan.edu.bancoengenharia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoEngenhariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
